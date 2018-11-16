874bb5d7713adb562182e4b0177659cc773357ab
Francesco Jo <nimbusob@gmail.com>
2018-11-16 21:35:26 +0900

    Migrated library module to jitpack.io repository

--- app_main/src/main/kotlin/com/github/fj/restapi/appconfig/JacksonDeserialisationModule.kt
+++ app_main/src/main/kotlin/com/github/fj/restapi/appconfig/JacksonDeserialisationModule.kt
@@ -4,8 +4,18 @@
  */
 package com.github.fj.restapi.appconfig
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.BeanProperty
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.github.fj.lib.util.ProtectedProperty
+import java.io.IOException
 
 /**
  * @author Francesco Jo(nimbusob@gmail.com)
@@ -14,9 +24,38 @@ import com.github.fj.lib.util.ProtectedProperty
 class JacksonDeserialisationModule : SimpleModule(
         JacksonDeserialisationModule::class.java.canonicalName) {
     init {
-        addSerializer(ProtectedProperty.ProtectedPropertyJacksonSerialiser())
+        addSerializer(ProtectedPropertyJacksonSerialiser())
 
-        addDeserializer(ProtectedProperty::class.java,
-                ProtectedProperty.ProtectedPropertyJacksonDeserialiser())
+        addDeserializer(ProtectedProperty::class.java, ProtectedPropertyJacksonDeserialiser())
+    }
+
+    class ProtectedPropertyJacksonSerialiser :
+            StdSerializer<ProtectedProperty<*>>(ProtectedProperty::class.java) {
+        override fun serialize(value: ProtectedProperty<*>, gen: JsonGenerator,
+                               serializers: SerializerProvider) {
+            serializers.defaultSerializeValue(value.value, gen)
+        }
+    }
+
+    class ProtectedPropertyJacksonDeserialiser :
+            JsonDeserializer<ProtectedProperty<*>>(), ContextualDeserializer {
+        private lateinit var valueType: JavaType
+
+        override fun createContextual(ctxt: DeserializationContext, property: BeanProperty):
+                JsonDeserializer<*> {
+            return ProtectedPropertyJacksonDeserialiser().apply {
+                this.valueType = property.type.containedType(0)
+            }
+        }
+
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext):
+                ProtectedProperty<*> {
+            return try {
+                ProtectedProperty<Any>(ctxt.readValue(p, valueType))
+            } catch (e: IOException) {
+                throw UnsupportedOperationException(
+                        "Unable to parse wrapped $valueType as JSON.", e)
+            }
+        }
     }
 }
